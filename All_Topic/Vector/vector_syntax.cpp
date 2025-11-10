886814a62f3dc71a61681b4f346923ffb57d68bc
Maria Akter Mukti <mariaaktermukti069@gmail.com>
2025-11-10 19:25:26 +0600

    Add files via upload

---
+++ All_Topic/Vector/vector_syntax.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    // vector<int> vec={1,2,3};
+    // cout<< vec[0]<< endl; //output->1
+
+    // vector<int> vec(5,0);
+    // for(int i:vec)
+    // {
+    //     cout<< i<< endl;
+    // }
+
+
+    //char vector
+    vector<char> ch={'a','e','i','o','u'};
+    for(int i:ch) //for each loop
+    {
+        cout << char(i) << endl;
+    }
+    return 0;
+}
