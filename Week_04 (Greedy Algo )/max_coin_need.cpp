becab4107c2de59e56e14326eea7b153f8c50cd1
Maria Akter Mukti <mariaaktermukti069@gmail.com>
2025-12-05 10:24:47 +0600

    Add files via upload

---
+++ Week_04 (Greedy Algo )/max_coin_need.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    vector<int> coins = {1,2,5,10,20,50,100,500,1000};
+    int V = 2524;
+    int count = 0;
+
+    sort(coins.begin(), coins.end());
+
+    for(int coin : coins){
+        if(V == 0) break;
+        int take = V / coin;
+        count += take;
+        V -= take * coin;
+        if(take > 0)
+            cout << coin << " coin: " << take << " times" << endl;
+    }
+
+    cout << "Maximum coins needed: " << count << endl;
+}
